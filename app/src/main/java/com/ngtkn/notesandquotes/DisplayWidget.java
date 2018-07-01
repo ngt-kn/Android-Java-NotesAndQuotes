46478dea3b074e392ad51847e1d05d18cde8cdeb
ngt kn <nagata.ken78@gmail.com>
2018-06-30 23:29:53 -0700

    add segue from widget to app

--- app/src/main/java/com/ngtkn/notesandquotes/DisplayWidget.java
+++ app/src/main/java/com/ngtkn/notesandquotes/DisplayWidget.java
@@ -1,8 +1,10 @@
 package com.ngtkn.notesandquotes;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -13,9 +15,14 @@ public class DisplayWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
         CharSequence widgetText = "...";
+
+        // Set up the pending intent for segue
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,intent,0);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.display_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
